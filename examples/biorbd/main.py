c6627e6e76f71e19f21407f57f77606ebc8be94e
Ipuch <pierre.puchaud@umontreal.ca>
2024-02-20 18:26:39 -0500

    pyorerun

--- examples/biorbd/main.py
+++ examples/biorbd/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from rrc3d import rrbiorbd
+from pyorerun import rrbiorbd
 
 
 def main():
--- examples/c3d/main.py
+++ examples/c3d/main.py
@@ -1,3 +1,3 @@
-from rrc3d import rrc3d
+from pyorerun import rrc3d
 
 rrc3d("example.c3d")
