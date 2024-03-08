557ec9d8a1d091570c8a2d656ec52e5224ee5b5f
Ipuch <pierre.puchaud@umontreal.ca>
2024-03-08 17:48:00 -0500

    Example(Animated model example complete)

--- examples/biorbd/msk_model.py
+++ examples/biorbd/msk_model.py
@@ -5,19 +5,23 @@ from pyorerun import BiorbdModel, PhaseRerun
 
 def main():
     # building some time components
-    nb_frames = 10
+    nb_frames = 50
     nb_seconds = 1
     t_span = np.linspace(0, nb_seconds, nb_frames)
 
-    model = BiorbdModel("models/shoulder_model.bioMod")
-    model2 = BiorbdModel("models/double_pendulum.bioMod")
+    model = BiorbdModel("models/Wu_Shoulder_Model_kinova_scaled_adjusted_2.bioMod")
+    # model2 = BiorbdModel("models/double_pendulum.bioMod")
     print(model.model)
     # building some generalized coordinates
     q = np.zeros((model.model.nbQ(), nb_frames))
-    q[0, :] = np.linspace(0, 0.02, nb_frames)
-    q[1, :] = np.linspace(0, 0.03, nb_frames)
-    q[2, :] = np.linspace(0, 0.04, nb_frames)
-    q[7, :] = np.linspace(0, 0.05, nb_frames)
+    # q[7, :] = np.linspace(0, np.pi / 2, nb_frames)
+    # q[9, :] = np.linspace(0, np.pi / 2, nb_frames)
+    q[10, :] = np.linspace(0, np.pi / 8, nb_frames)
+    q[12, :] = np.linspace(0, np.pi / 3, nb_frames)
+    q[11, :] = np.linspace(0, np.pi / 4, nb_frames)
+    q[13, :] = np.linspace(0, np.pi / 8, nb_frames)
+    q[14, :] = np.linspace(0, np.pi / 8, nb_frames)
+    q[15, :] = np.linspace(0, np.pi / 8, nb_frames)
 
     viz = PhaseRerun(t_span)
     # viz.add_animated_model(model2, q[:2, :])
