f7c3177d7d4b71c9058d76746df1ba94026a6b39
Ipuch <pierre.puchaud@umontreal.ca>
2024-03-12 13:47:54 -0400

    clean(remove line in msk example)

--- examples/biorbd/msk_model.py
+++ examples/biorbd/msk_model.py
@@ -10,12 +10,9 @@ def main():
     t_span = np.linspace(0, nb_seconds, nb_frames)
 
     model = BiorbdModel("models/Wu_Shoulder_Model_kinova_scaled_adjusted_2.bioMod")
-    # model2 = BiorbdModel("models/double_pendulum.bioMod")
-    print(model.model)
+
     # building some generalized coordinates
     q = np.zeros((model.model.nbQ(), nb_frames))
-    # q[7, :] = np.linspace(0, np.pi / 2, nb_frames)
-    # q[9, :] = np.linspace(0, np.pi / 2, nb_frames)
     q[10, :] = np.linspace(0, np.pi / 8, nb_frames)
     q[12, :] = np.linspace(0, np.pi / 3, nb_frames)
     q[11, :] = np.linspace(0, np.pi / 4, nb_frames)
@@ -24,7 +21,6 @@ def main():
     q[15, :] = np.linspace(0, np.pi / 8, nb_frames)
 
     viz = PhaseRerun(t_span)
-    # viz.add_animated_model(model2, q[:2, :])
     viz.add_animated_model(model, q)
     viz.rerun("msk_model")
 
