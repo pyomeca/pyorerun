2209ddad405ce9094076d6975f6514a434584b00
Ipuch <pierre.puchaud@umontreal.ca>
2024-03-26 18:21:29 -0400

    tests

--- tests/test_ligament.py
+++ tests/test_ligament.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from pyorerun.biorbd_components.ligaments import BiorbdModelLigaments, LineStripProperties
+from pyorerun.biorbd_components.ligaments import LineStripProperties, LigamentsUpdater
 
 
 def dummy_callable_ligaments(q):
@@ -17,12 +17,12 @@ def test_biorbd_model_ligaments():
     np.testing.assert_almost_equal(ligament_properties.color_to_rerun(), np.array([[255, 0, 0], [255, 0, 0]]))
     np.testing.assert_almost_equal(ligament_properties.radius_to_rerun(), np.array([0.5, 0.5]))
 
-    ligaments = BiorbdModelLigaments("test", ligament_properties, dummy_callable_ligaments)
+    ligaments = LigamentsUpdater("test", ligament_properties, dummy_callable_ligaments)
 
     # Test initialization
     assert ligaments.name == "test/ligaments"
-    assert ligaments.ligament_properties == ligament_properties
-    assert ligaments.callable_ligament == dummy_callable_ligaments
+    assert ligaments.properties == ligament_properties
+    assert ligaments.update_callable == dummy_callable_ligaments
 
     # Test nb_strips property
     assert ligaments.nb_strips == 2
