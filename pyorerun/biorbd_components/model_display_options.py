6a8c602ea86485d56075164ec05a95056fe62b69
Charbie <eve.charbonneau.1@umontreal.ca>
2024-04-05 14:29:20 -0400

    temporary flags

--- pyorerun/biorbd_components/model_display_options.py
+++ pyorerun/biorbd_components/model_display_options.py
@@ -16,6 +16,15 @@ class DisplayModelOptions:
     muscles_color: tuple[int, int, int] = (255, 0, 0)
     muscles_radius: float = 0.004
 
-    # NOTE : mesh_opacity doesnt exist in rerun yet
+    # NOTE : mesh_opacity doesn't exist in rerun yet
     # segment_frame_size: float = 0.1 not implemented yet
     mesh_color: tuple[int, int, int] = (255, 255, 255)
+
+    background_color: tuple[int, int, int] = (15, 15, 15)
+
+    # Flags to display the different components
+    show_markers: bool = True
+    show_marker_labels: bool = True
+    show_mesh: bool = True
+    show_global_ref_frame: bool = True
+    show_local_ref_frame: bool = True
