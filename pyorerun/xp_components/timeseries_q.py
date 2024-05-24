b7d92d8530bd0feabffbd71363fc065f46244d2a
Ipuch <pierre.puchaud@umontreal.ca>
2024-05-24 08:27:35 -0400

    feat: no range when needed to plot q

--- pyorerun/xp_components/timeseries_q.py
+++ pyorerun/xp_components/timeseries_q.py
@@ -29,20 +29,32 @@ class TimeSeriesQ(ExperimentalData):
         return 1
 
     def to_rerun(self, frame: int) -> None:
-        for joint_idx in range(self.nb_q):
-            name = f"{self.properties.displayed_joint_names[joint_idx]}"
-            qmin, qmax = self.properties.ranges[joint_idx]
-            #  todo: this log calls should be done once for all somewhere in the properties of Q
-            rr.log(
-                f"{name}/min", rr.SeriesLine(color=self.properties.min_color, name="min", width=self.properties.width)
-            )
-            rr.log(
-                f"{name}/max", rr.SeriesLine(color=self.properties.max_color, name="max", width=self.properties.width)
-            )
-            rr.log(
-                f"{name}/value", rr.SeriesLine(color=self.properties.value_color, name="q", width=self.properties.width)
-            )
-            self.to_serie_line(name=name, min=qmin, max=qmax, val=self.q[joint_idx, frame])
+        if self.properties.ranges is None:
+            for joint_idx in range(self.nb_q):
+                name = f"{self.properties.displayed_joint_names[joint_idx]}"
+                rr.log(
+                    f"{name}/value",
+                    rr.SeriesLine(color=self.properties.value_color, name="q", width=self.properties.width),
+                )
+                rr.log(f"{name}/value", rr.Scalar(self.q[joint_idx, frame]))
+        else:
+            for joint_idx in range(self.nb_q):
+                name = f"{self.properties.displayed_joint_names[joint_idx]}"
+                qmin, qmax = self.properties.ranges[joint_idx]
+                #  todo: this log calls should be done once for all somewhere in the properties of Q
+                rr.log(
+                    f"{name}/min",
+                    rr.SeriesLine(color=self.properties.min_color, name="min", width=self.properties.width),
+                )
+                rr.log(
+                    f"{name}/max",
+                    rr.SeriesLine(color=self.properties.max_color, name="max", width=self.properties.width),
+                )
+                rr.log(
+                    f"{name}/value",
+                    rr.SeriesLine(color=self.properties.value_color, name="q", width=self.properties.width),
+                )
+                self.to_serie_line(name=name, min=qmin, max=qmax, val=self.q[joint_idx, frame])
 
     @staticmethod
     def to_serie_line(name: str, min: float, max: float, val: float):
