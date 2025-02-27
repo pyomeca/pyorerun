7c64ba4d24585e8d00793afadcb42637df4bf7a3
Ipuch <pierre.puchaud@umontreal.ca>
2025-02-27 12:07:47 -0500

    renaming for clarity

--- examples/biorbd/gait_reconstruction.py
+++ examples/biorbd/gait_reconstruction.py
@@ -1,4 +1,5 @@
 import pickle
+
 from pyomeca import Markers as PyoMarkers
 
 from pyorerun import BiorbdModel, PhaseRerun
@@ -7,42 +8,42 @@ from pyorerun import BiorbdModel, PhaseRerun
 def main():
 
     # --- Load the experimental data from file --- #
-    with open(f"data/reduced_exp_data.pkl", "rb") as file:
+    with open(f"data/gait_reconstruction.pkl", "rb") as file:
         data = pickle.load(file)
-        q_reduced = data["q_reduced"]
-        t_reduced = data["t_reduced"]
+        q = data["q"]
+        t_span = data["t_span"]
         force_plate_1_corners = data["force_plate_1_corners"]
         force_plate_2_corners = data["force_plate_2_corners"]
-        f_ext_reduced_1 = data["f_ext_reduced_1"]
-        f_ext_reduced_2 = data["f_ext_reduced_2"]
-        markers_reduced = data["markers_reduced"]
+        f_ext_1 = data["f_ext_1"]
+        f_ext_2 = data["f_ext_2"]
+        markers = data["markers"]
 
     # --- Create the visualization --- #
     # Add the model
     biorbd_model_path = "models/walker.bioMod"
     model = BiorbdModel(biorbd_model_path)
     model.options.transparent_mesh = False
-    viz = PhaseRerun(t_reduced)
+    viz = PhaseRerun(t_span)
 
     # Add experimental markers
-    markers = PyoMarkers(data=markers_reduced, channels=list(model.marker_names))
+    pyomarkers = PyoMarkers(data=markers, channels=list(model.marker_names))
 
     # Add force plates to the animation
     viz.add_force_plate(num=0, corners=force_plate_1_corners)
     viz.add_force_plate(num=1, corners=force_plate_2_corners)
     viz.add_force_data(
         num=0,
-        force_origin=f_ext_reduced_1[:3, :],
-        force_vector=f_ext_reduced_1[6:9, :],
+        force_origin=f_ext_1[:3, :],
+        force_vector=f_ext_1[6:9, :],
     )
     viz.add_force_data(
         num=1,
-        force_origin=f_ext_reduced_2[:3, :],
-        force_vector=f_ext_reduced_2[6:9, :],
+        force_origin=f_ext_2[:3, :],
+        force_vector=f_ext_2[6:9, :],
     )
 
     # Add the kinematics
-    viz.add_animated_model(model, q_reduced, tracked_markers=markers)
+    viz.add_animated_model(model, q, tracked_markers=pyomarkers)
 
     # Play
     viz.rerun("Experimental data with kinematics reconstruction")
