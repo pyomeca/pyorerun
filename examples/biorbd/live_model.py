4106fabeb413d4cfddf1fc43d243a3e8719ded67
Ipuch <pierre.puchaud@umontreal.ca>
2025-06-11 17:18:51 +0200

    big push

--- examples/biorbd/live_model.py
+++ examples/biorbd/live_model.py
@@ -10,15 +10,19 @@ import numpy as np
 
 
 model_path = "models/Wu_Shoulder_Model_kinova_scaled_adjusted_2.bioMod"
-import rerun as rr
-import numpy as np
-from pyorerun import ModelUpdater
+animation = LiveModelAnimation(model_path, with_q_charts=True)
+animation.rerun()
 
-model = ModelUpdater.from_file(model_path)
-rr.init("my_thing", spawn=True)
-# rr.log("test", timeless=True)
-q = np.zeros(16)
-for i in range(100):
-    rr.set_time_sequence(timeline="step", sequence=i)
-    model.to_rerun(q)
-# rr.log("anything", rr.Anything())
+# Amedeo broke everything haha.
+# import rerun as rr
+# import numpy as np
+# from pyorerun import ModelUpdater
+#
+# model = ModelUpdater.from_file(model_path)
+# rr.init("my_thing", spawn=True)
+# # rr.log("test", timeless=True)
+# q = np.zeros(16)
+# for i in range(100):
+#     rr.set_time_sequence(timeline="step", sequence=i)
+#     model.to_rerun(q)
+# # rr.log("anything", rr.Anything())
--- examples/osim/from_osim_model.py
+++ examples/osim/from_osim_model.py
@@ -1,22 +1,19 @@
-import os
 import opensim
-import numpy as np
 
 from pyorerun import OsimModel, PhaseRerun, OsimTimeSeries, DisplayModelOptions
 
 
 def main():
-    osim_model = opensim.Model(r"models\Rajagopal2015.osim")
+    osim_model = opensim.Model(r"Rajagopal2015.osim")
     display_options = DisplayModelOptions()
-    display_options.mesh_path = "D:\Documents\Programmation\pyorerun\examples\biorbd\models\Geometry_cleaned"
+    display_options.mesh_path = "../biorbd/models/Geometry_cleaned"
     prr_model = OsimModel.from_osim_object(osim_model, options=display_options)
 
     mot_file = r"ik.mot"
     mot_time_series = OsimTimeSeries(mot_file, osim_model)
+
     viz = PhaseRerun(mot_time_series.times)
     viz.add_animated_model(prr_model, mot_time_series.q_in_radian)
-    # viz = PhaseRerun(t_span=t_span)
-    # viz.add_animated_model(prr_model, q)
     viz.rerun("msk_model")
 
 
